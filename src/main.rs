e238401e38c67b4cf0552336df738c693c5765e1
confused-ace-noises <alt.alt.24.02@gmail.com>
2024-07-25 19:25:47 +0200

    added errorr messages TODO: error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use core::panic;
 use std::io;
+use std::process;
 
 trait Check {
-    fn check(&self) -> Vec<u32>;
+    fn check(&self, checklen: usize) -> Result<Vec<u32>, String>;
 }
 
 impl Check for String {
-    fn check(&self) -> Vec<u32>{
-        return self.trim()
-        .split(",")
-        .filter(|s| s.trim()
-            .chars()
-            .all(|s| s.is_digit(10)))
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u32>>();
+    fn check(&self, checklen: usize) -> Result<Vec<u32>, String>{
+        let it = self.trim().split(",").collect::<Vec<&str>>();
+
+        if it.len() > checklen {
+            return Err(String::from("sono stati immessi più di") + &checklen.to_string() + "parametri/o")
+        } else if it.len() < checklen {
+            return Err(String::from("sono stati immessi meno di") + &checklen.to_string() + " parametri");
+        } 
+
+        let mut tracker = 0;
+        for i in &it {
+            tracker +=1;
+            
+            if ! i.trim().chars().all(|s| s.is_digit(10)) {
+                let error = "il ".to_string() + &tracker.to_string() + "o numero contiene caratteri non-numerici.";
+                return Err(error)
+            };
+        }
+
+        return Ok(it.into_iter().map(|s| s.parse().unwrap()).collect::<Vec<u32>>());
     }
 }
 
@@ -22,13 +35,19 @@ fn main() {
     println!("seleziona una modalità: \n\t1: MARGINE <-- prezzo e costo. \n\t2: PREZZO <-- costo e margine. \n\t3: manuale");
     let mut input_start = String::new();
     io::stdin().read_line(&mut input_start).expect("something went worng");
-    let input_start = input_start.check()[0];
+    let input_start = match input_start.check(1) {
+        Ok(r) => r[0],
+        Err(e) => process::exit(1) 
+    };
 
     if input_start == 1 {
         println!("inserire il PREZZO e il COSTO separati da una virgola: \n\t\tPREZZO,COSTO");
         let mut input_mod_1 = String::new();
         io::stdin().read_line(&mut input_mod_1).expect("somethign went wrong");
-        let input_mod_1 = input_mod_1.check();
+        let input_mod_1 = match input_mod_1.check(2) {
+            Ok(r) => r,
+            Err(e) => std::process::exit(1),
+        };
 
         match input_mod_1.len(){
             2 => {
@@ -57,7 +76,10 @@ fn main() {
         println!("inserire il COSTO e il MARGINE separati da una virgola: \n\t\tCOSTO,MARGINE");
         let mut input_mod_1 = String::new();
         io::stdin().read_line(&mut input_mod_1).expect("somethign went wrong");
-        let input_mod_1 = input_mod_1.check();
+        let input_mod_1 = match input_mod_1.check(2) {
+            Ok(r) => r,
+            Err(e) => std::process::exit(1),
+        };
 
         match input_mod_1.len(){
             2 => {
